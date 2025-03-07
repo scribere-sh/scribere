cf037df7d8f6c4a87b765dbe6a4317da2b42ed06
d3rpp <github@d3rpp.dev>
2025-03-08 11:44:32 +1300

    removed unused cryptography code

--- apps/app/src/lib/server/auth/cryptography.ts
+++ apps/app/src/lib/server/auth/cryptography.ts
@@ -1,45 +1,6 @@
-import { ENCRYPTION_KEY } from '$env/static/private';
-import { DynamicBuffer } from '@oslojs/binary';
-import { decodeBase32IgnorePadding, decodeBase64, encodeBase32LowerCaseNoPadding } from '@oslojs/encoding';
-import { createCipheriv, createDecipheriv } from 'crypto';
+import { decodeBase32IgnorePadding, encodeBase32LowerCaseNoPadding } from '@oslojs/encoding';
 
 import * as Argon2 from 'argon2';
-
-const key = decodeBase64(ENCRYPTION_KEY);
-
-export const encrypt = (data: Uint8Array): Uint8Array => {
-	const iv = new Uint8Array(16);
-	crypto.getRandomValues(iv);
-
-	const cipher = createCipheriv('aes-128-gcm', key, iv);
-	const encrypted = new DynamicBuffer(0);
-	encrypted.write(iv);
-	encrypted.write(cipher.update(data));
-	encrypted.write(cipher.final());
-	encrypted.write(cipher.getAuthTag());
-	return encrypted.bytes();
-};
-
-export const encryptString = (data: string): Uint8Array => {
-	return encrypt(new TextEncoder().encode(data));
-};
-
-export const decrypt = (encrypted: Uint8Array): Uint8Array => {
-	if (encrypted.length < 33) {
-		throw new Error('invalid data');
-	}
-
-	const decipher = createDecipheriv('aes-128-gcm', key, encrypted.slice(0, 16));
-	decipher.setAuthTag(encrypted.slice(encrypted.byteOffset - 16));
-	const decrypted = new DynamicBuffer(0);
-	decrypted.write(decipher.update(encrypted.slice(16, encrypted.byteLength - 16)));
-	decrypted.write(decipher.final());
-	return decrypted.bytes();
-};
-
-export const decryptString = (data: Uint8Array): string => {
-	return new TextDecoder().decode(decrypt(data));
-};
 
 export const generateTokenBytes = (length: number = 32) => {
 	const bytes = new Uint8Array(length);
